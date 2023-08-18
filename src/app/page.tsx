81189c7236365ec7795e6b859f80dc8611e27a0a
siffogh <seif@tinloof.com>
2023-08-18 10:25:59 +0200

    No cookie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import { cookies } from 'next/headers';
-
 export const runtime = 'edge';
 
 export default async function Home() {
   const data = await fetch('https://hub.dummyapis.com/delay?seconds=5', {
-    cache: 'force-cache',
-    next: { revalidate: 1 },
+    next: {
+      revalidate: 1,
+    },
   });
-  const foo = cookies().get('foo');
   return (
     <code>
       <pre>{JSON.stringify(await data.text(), null, 2)}</pre>
